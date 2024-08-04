f1a330cf3f6ffcb1568e86ba730efb0293bd0dc5
hemanth2k42 <161370698+hemanth2k42@users.noreply.github.com>
2024-08-04 18:04:39 +0530

    Update mint.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,8 +1,8 @@
-// This script batch mints criceket ERC721A tokens.
+// This script batch mints cricket ERC721A tokens.
 
 // Import required libraries
 const { ethers } = require("hardhat"); // Hardhat is a development environment for Ethereum smart contracts
-require("dotenv").config();// Load environment variables from a .env file
+require("dotenv").config(); // Load environment variables from a .env file
 
 async function main() {
   // Get private key from environment variables (Make sure PRIVATE_KEY is defined in the .env file)
@@ -10,7 +10,7 @@ async function main() {
 
   // The URL of the network provider (Goerli test network in this case)
   const networkAddress =
-    "https://eth-goerli.g.alchemy.com/v2/Bh22s-iYGmFwy-9Dq3New4jIpUES9xZt";
+    "https://rpc2.sepolia.org/";
 
   // Create a provider using the specified URL to connect to the Ethereum network
   const provider = new ethers.providers.JsonRpcProvider(networkAddress);
@@ -19,22 +19,24 @@ async function main() {
   const signer = new ethers.Wallet(privateKey, provider);
 
   // The address of the deployed contract that we want to interact with
-  const contractAddress = "0xaD8c1EA2fB5Be02DD73E497aB1DdD5EDD5f27C82";
+  const contractAddress = "0xac311515a5A7d0c88b4b089c3fb69221A68Ec7EE";
 
   // Get the contract factory for the MyCollectibles contract and attach it to the signer
-  const OneNFT = await ethers.getContractFactory("MyColleccurl -O <file_url>
+  const MyCollectibles = await ethers.getContractFactory("MyCollectibles", signer);
 
+  // Attach the deployed contract to the signer
+  const contract = MyCollectibles.attach(contractAddress);
 
   // Call the mint function on the contract to mint 5 tokens
   await contract.mint(5);
 
   // Log a message to the console to indicate that the tokens have been minted
-  console.log("Minted 05 tokens.");
+  console.log("Minted 5 tokens.");
 }
 
 main()
-  .then(() => process.exit(0))// Exit the process with success status code (0) after execution
+  .then(() => process.exit(0)) // Exit the process with success status code (0) after execution
   .catch((error) => {
     console.error(error);
-    process.exit(1);// Log any errors that occur during execution and exit the process with an error status code (1)
-  }); 
+    process.exit(1); // Log any errors that occur during execution and exit the process with an error status code (1)
+  });
